96f56774c07ddf4c5665c6c8614bf98b5e1e7f37
Sara-Barbi <barbisara02@gmail.com>
2021-12-10 22:46:33 +0100

    elseif&id

--- myscript.js
+++ myscript.js
@@ -1,21 +1,18 @@
-let blocco = document.querySelector(".myblock");
+let blocco = document.getElementById("myblock");
 
-for(let index = 1; index <= 100; index++) {
-   if(index%3 == 0 )and(index%5 == 0){
-        let element = `<div class= "fizzbuzz">FIZZBUZZ${index}</div >`
-        blocco.innerHTML += element ;    
-}else (index%3 == 0) {
-    let element = `<div class= "tree">FIZZ${index}</div >`
-    blocco.innerHTML += element ;    
-} else (index%5 == 0) {
-    let element = `<div class= "five">BUZZ${index}</div >`
-    blocco.innerHTML += element ;    
-}else{
-    let element = `<div class= "mystyle">${index}</div >`
-    blocco.innerHTML += element ;    
-}
+for(let index = 1; index <= 100; index++){
+   
+    if( (index%3 == 0) && (index%5 == 0) ){
+        blocco.innerHTML +=  `<div class=" mystyle fizzbuzz">FIZZBUZZ</div>`;
 
+    } else if(index%3 == 0) {
+        blocco.innerHTML +=  `<div class="mystyle tree">FIZZ</div>`;
 
-    let element = `<div class= "fizzbuzz">FizzBuzz</div >`
-    blocco.innerHTML += element ; 
+    } else if(index%5 == 0) {
+        blocco.innerHTML +=  `<div class="mystyle five">BUZZ</div>`;
+
+    }else{
+        blocco.innerHTML +=  `<div class="mystyle">`+ index +`</div>`;
+
+    }
 }
