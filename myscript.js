a5d37f9a0d224db0b10ce96761a69a8808e535fb
Sara-Barbi <barbisara02@gmail.com>
2021-12-10 20:51:40 +0100

    blocchi tutti blu

--- myscript.js
+++ myscript.js
@@ -1,6 +1,7 @@
-let blocchi = document.getElementById("myblock");
+let blocco = document.querySelector(".myblock");
 
-for(let index = 1; index >=100; index++){
-   let element = `<li class= mybox mybox-${ }
+for(let index = 1; index <= 100; index++) {
+   let element = `<div class= "mystyle">${index}</div >`
+   blocco.innerHTML += element ;
+   
 }
-
