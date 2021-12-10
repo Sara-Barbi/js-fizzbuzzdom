f544fd2aa0388ee51806cd51f1bd20723a160c56
Sara-Barbi <barbisara02@gmail.com>
2021-12-10 21:12:44 +0100

    else

--- myscript.js
+++ myscript.js
@@ -1,7 +1,21 @@
 let blocco = document.querySelector(".myblock");
 
 for(let index = 1; index <= 100; index++) {
-   let element = `<div class= "mystyle">${index}</div >`
-   blocco.innerHTML += element ;
-   
+   if(index%3 == 0 )and(index%5 == 0){
+        let element = `<div class= "fizzbuzz">FIZZBUZZ${index}</div >`
+        blocco.innerHTML += element ;    
+}else (index%3 == 0) {
+    let element = `<div class= "tree">FIZZ${index}</div >`
+    blocco.innerHTML += element ;    
+} else (index%5 == 0) {
+    let element = `<div class= "five">BUZZ${index}</div >`
+    blocco.innerHTML += element ;    
+}else{
+    let element = `<div class= "mystyle">${index}</div >`
+    blocco.innerHTML += element ;    
+}
+
+
+    let element = `<div class= "fizzbuzz">FizzBuzz</div >`
+    blocco.innerHTML += element ; 
 }
