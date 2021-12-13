412ad10e31ce6f3d7e216398de6c1a3b42774be3
Sara-Barbi <barbisara02@gmail.com>
2021-12-13 11:31:13 +0100

    comments

--- myscript.js
+++ myscript.js
@@ -1,9 +1,9 @@
-let blocco = document.getElementById("myblock");
+let blocco = document.getElementById("myblock"); //richiamo l'elemento dell'html
 
-for(let index = 1; index <= 100; index++){
+for(let index = 1; index <= 100; index++){ //stabilisco un contatore all'interno di for
    
-    if( (index%3 == 0) && (index%5 == 0) ){
-        blocco.innerHTML +=  `<div class=" mystyle fizzbuzz">FIZZBUZZ</div>`;
+    if( (index%3 == 0) && (index%5 == 0) ){ //stabilisco la condizione in if dentro for
+        blocco.innerHTML +=  `<div class=" mystyle fizzbuzz">FIZZBUZZ</div>`; //aggiungiamo la stringa che ci interessa a querlla condizione METTERE += PER GENERARE PIù DI UN ELEMENTO
 
     } else if(index%3 == 0) {
         blocco.innerHTML +=  `<div class="mystyle tree">FIZZ</div>`;
